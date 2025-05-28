8e85a2fcf78e97020613256ee0de1841aa248031
ThuyTrang1604 <trangptt22411c@st.uel.edu.vn>
2025-05-28 18:53:32 +0700

    assignment2805

---
+++ PhanThiThuyTrang_K224111470_K22411c/app/src/main/java/com/example/mynewapplication/Product.java
@@ -0,0 +1,68 @@
+package com.example.mynewapplication;
+
+public class Product {
+    private int id;
+    private String productCode;
+    private String productName;
+    private double unitPrice;
+    private String imageLink;
+
+    public Product(int id, String productCode, String productName, double unitPrice, String imageLink) {
+        this.id = id;
+        this.productCode = productCode;
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.imageLink = imageLink;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", productCode='" + productCode + '\'' +
+                ", productName='" + productName + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", imageLink='" + imageLink + '\'' +
+                '}';
+    }
+} 
